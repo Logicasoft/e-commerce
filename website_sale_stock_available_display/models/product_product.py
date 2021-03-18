644fde49d797bd52b90b7d4531a38c82db2dd784
Jon_Giaco <jonathan.giacomello@logicasoft.eu>
2021-03-18 14:33:35 +0100

    [20040] Correction

---
+++ website_sale_stock_available_display/models/product_product.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2013 LogicaSoft SPRL (<http://www.logicasoft.eu>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from odoo import models, fields, api
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    def action_product_product_free_qty(self):
+        self.ensure_one()
+        action = self.env["ir.actions.actions"]._for_xml_id('stock.stock_replenishment_product_product_action')
+        return action
